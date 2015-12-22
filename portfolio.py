53c0af2f750c53c8a1967c6e660b5e01d9815699
parth9615 <parth9615@gmail.com>
2015-12-21 21:51:51 -0500

    fixed bugs in reading and writing to and from files

--- portfolio.py
+++ portfolio.py
@@ -10,11 +10,25 @@ import re
 def saveToFile(portfolio):
     removeFileIfExists('info.txt')
     f = open('info.txt', 'a')
-    print portfolio
     for key in portfolio:
-        string = key + ": " + str(portfolio[key]) + '\n'
+        # value = portfolio[key][0]
+        # ticker = value[0] + " , "
+        # date = value[1] + " , "
+        # price = value[2] + " , "
+        # numbers = value[3]
+        # string = key +": " + ticker + date + price + numbers + '\n'
+        string = getValueToWrite(key, portfolio[key][0])
         f.write(string)
 
+"""
+
+"""
+def getValueToWrite(key, value):
+    strList = key + ' : '
+    for i in range(len(value) - 1):
+        strList += ' ' + value[i] + ' , '
+    strList+= ' ' + value[i+1] + '\n'
+    return strList
 
 """
     This method removes the file specifed from the computer
@@ -25,23 +39,26 @@ def removeFileIfExists(name):
         os.remove(path)
 
 
-""""
+"""
     This method reads the file info.txt and saves all the portfolio information
     for all the users in an instance of portfolio
-""""
+"""
 def readFromFile():
-    f = open('read.txt' , 'rU')
-    rawText = f.readlines()
-    for x in rawText:
-        y = x.split(":")
-        portfolio[y[0]] = removeCharactersAndGetValues(y[1])
+    path = os.getcwd() + "/info.txt"
+    if os.path.exists(path):
+        f = open('info.txt' , 'rU')
+        rawText = f.readlines()
+        for x in rawText:
+            y = x.split(":")
+            portfolio[y[0]] = removeCharactersAndGetValues(y[1])
 
 
 """
     This method takes a  string and remvoes all the formatting from it and
     returns the stock infromation for the portfolio
 """
-def removeCharactersAndGetValues(string):
+def removeCharactersAndGetValues(value):
+    string = value.replace(" " , "")
     s1 = string.replace('[' , '')
     s2 = s1.replace(']' , '')
     s3 = s2.replace('(' , '')
@@ -55,10 +72,10 @@ def removeCharactersAndGetValues(string):
     return tupList
 
 
-""""
+"""
     This method takes a a key and value and adds it
     to the portfolio and formats it properly
-""""
+"""
 def addToPortfolio(key,value):
     if key in portfolio:
         portfolio[key].append(value)
@@ -67,17 +84,20 @@ def addToPortfolio(key,value):
         ls.append(value)
         portfolio[key] = ls
 
-
+"""
+    Main runner method.
+"""
 def main():
+    readFromFile()
+    for item in portfolio:
+        print item , portfolio[item]
     while (True):
         name = raw_input("Enter Profile name: ")
         if name != 'quit':
             while(True):
                 ticker = raw_input('enter ticker(quit to quit): ')
-                # quote = trader.getCurrentPriceFromTicker(ticker)
-                quote = "CHANGE THIS WHEN INTERNET CONNECTION"
-                #if quote: RESTORE THIS WHEN INTERNET CONNECTION
-                if ticker != 'quit':
+                quote = trader.getCurrentPriceFromTicker(ticker)
+                if quote:
                     price = raw_input("Enter price: ")
                     numbers = raw_input("Enter quantity: ")
                     date = raw_input("Enter date: (YYYY-MM-DD): ")
@@ -93,6 +113,4 @@ def main():
 portfolio = {}
 
 if __name__ == '__main__':
-  # removeFileIfExists()
-  # main()
-  readFromFile()
+    main()
